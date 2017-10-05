2478203936db980212b884935773f9c552098fef
movsumlutabriz <movsumlutab@gmail.com>
2017-10-05 16:12:15 +0300

    Create screen-greeting.js

---
+++ js/screen-greeting.js
@@ -0,0 +1,31 @@
+import {show as showScreen} from "./screen";
+import {elemFromTemplate} from "./elem";
+import screenRules from "./screen-rules";
+
+const html = `<div class="greeting central--blur">
+  <div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
+  <h1 class="greeting__asterisk">*</h1>
+  <div class="greeting__challenge">
+    <h3>Лучшие художники-фотореалисты бросают&nbsp;тебе&nbsp;вызов!</h3>
+    <p>Правила игры просты.<br>
+      Нужно отличить рисунок&nbsp;от фотографии и сделать выбор.<br>
+      Задача кажется тривиальной, но не думай, что все так просто.<br>
+      Фотореализм обманчив и коварен.<br>
+      Помни, главное — смотреть очень внимательно.</p>
+  </div>
+  <div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>
+</div>`;
+
+const elem = elemFromTemplate(html);
+
+elem.querySelector(`.greeting__continue`).addEventListener(`click`, () => showScreen(screenRules));
+
+document.addEventListener(`click`, (e) => {
+  if (e.target.classList.contains(`back`)) {
+    showScreen(elem);
+  } else if (e.target.parentNode && e.target.parentNode.classList.contains(`back`)) {
+    showScreen(elem);
+  }
+});
+
+export default elem;
