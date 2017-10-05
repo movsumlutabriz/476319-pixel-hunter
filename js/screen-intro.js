c9b25f507bd4382449fba4ac149d402209cd0402
movsumlutabriz <movsumlutab@gmail.com>
2017-10-05 16:10:59 +0300

    Create screen-intro.js

---
+++ js/screen-intro.js
@@ -0,0 +1,15 @@
+import {elemFromTemplate} from "./elem";
+import {show as showScreen} from "./screen";
+import screenGreeting from "./screen-greeting";
+
+const html = `<div id="intro" class="intro">
+  <h1 class="intro__asterisk">*</h1>
+  <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</div>`;
+
+
+const elem = elemFromTemplate(html);
+
+elem.querySelector(`.intro__asterisk`).addEventListener(`click`, () => showScreen(screenGreeting));
+
+export default elem;
