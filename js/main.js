56d39dbde2530739fa3d49fe494a114f4b9dfb59
movsumlutabriz <movsumlutab@gmail.com>
2017-10-09 15:55:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,4 @@
-const screenMain = document.querySelector(`main.central`);
+import {show as showScreen} from "./screen";
+import screenIntro from "./screen-intro";
 
-const screens = Array.from(document.getElementsByTagName(`template`)).map((template) => {
-  const div = document.createElement(`div`);
-  div.appendChild(template.content);
-  return div;
-});
-
-let currentScreenIndex = null;
-
-const showScreen = (screenIndex) => {
-  if (!screens[screenIndex]) {
-    return;
-  }
-
-  screenMain.innerHTML = ``;
-  screenMain.appendChild(screens[screenIndex]);
-
-  currentScreenIndex = screenIndex;
-};
-
-const screenForward = () => {
-  showScreen(currentScreenIndex + 1);
-};
-
-const screenBack = () => {
-  showScreen(currentScreenIndex - 1);
-};
-
-showScreen(0);
-
-document.addEventListener(`keydown`, (e) => {
-  const keyRight = 39;
-  const keyLeft = 37;
-
-  if (e.altKey) {
-    switch (e.keyCode) {
-      case keyRight:
-        screenForward();
-        break;
-      case keyLeft:
-        screenBack();
-        break;
-    }
-  }
-});
+showScreen(screenIntro);
