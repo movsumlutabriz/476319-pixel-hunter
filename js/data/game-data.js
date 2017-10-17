54fb32c7d0aa55acbf0994d535715fc4191a68e4
Tabriz Movsumlu <movsumlutab@gmail.com>
2017-10-17 14:35:03 +0400

    задание 3.2 исправление ошибок

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -1,20 +1,30 @@
-export const scoring = (answers, lives) => {
+export const getScoring = (answers, lives) => {
   if (answers.length < 10) {
     return -1;
   }
-  let scores = lives * 50;
-  answers.forEach((answer) => {
-    switch (answer) {
-      case `slow`:
-        scores += 50;
-        break;
-      case `middle`:
-        scores += 100;
-        break;
-      case `fast`:
-        scores += 150;
-        break;
+  let score = lives * 50;
+  answers.forEach((item) => {
+    if (item.right) {
+      score += 100;
+    }
+    if (item.time < 10) {
+      score -= 50;
+    }
+    if (item.time > 20) {
+      score += 50;
     }
   });
-  return scores;
+  return score;
+};
+
+export const getTimer = (time) => {
+  return {
+    tick: () => {
+      time--;
+      if (time <= 0) {
+        return `end`;
+      }
+      return time;
+    }
+  };
 };
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import {scoring} from './game-data';
+import {getScoring, getTimer} from './game-data';
 
 describe(`Array`, () => {
   describe(`#indexOf()`, () => {
@@ -9,20 +9,80 @@ describe(`Array`, () => {
   });
 });
 
-describe(`Scoring`, () => {
+describe(`getScoring`, () => {
   it(`Если игрок ответил меньше чем на 10 вопросов, то игра считается непройденой и функция должна вернуть -1`, () => {
-    assert.equal(-1, scoring([], 3));
+    assert.equal(-1, getScoring([], 3));
   });
   it(`Если игрок ответил на все вопросы и не быстро и не медленно и у него остались все жизни, то функция должна вернуть 1150 очков`, () => {
-    assert.equal(1150, scoring([`middle`, `middle`, `middle`, `middle`, `middle`, `middle`, `middle`, `middle`, `middle`, `middle`], 3));
+    assert.equal(1150, getScoring([
+      {right: true, time: 11},
+      {right: true, time: 17},
+      {right: true, time: 18},
+      {right: true, time: 15},
+      {right: true, time: 13},
+      {right: true, time: 12},
+      {right: true, time: 11},
+      {right: true, time: 17},
+      {right: true, time: 15},
+      {right: true, time: 16}
+    ], 3));
   });
   it(`Если все ответы быстрые, остались все жизни, функция возвращает 1650`, () => {
-    assert.equal(1650, scoring([`fast`, `fast`, `fast`, `fast`, `fast`, `fast`, `fast`, `fast`, `fast`, `fast`], 3));
+    assert.equal(1650, getScoring([
+      {right: true, time: 21},
+      {right: true, time: 27},
+      {right: true, time: 28},
+      {right: true, time: 25},
+      {right: true, time: 23},
+      {right: true, time: 22},
+      {right: true, time: 21},
+      {right: true, time: 27},
+      {right: true, time: 25},
+      {right: true, time: 26}
+    ], 3));
   });
   it(`Если все ответы медленные, остались все жизни, функция возвращает 650`, () => {
-    assert.equal(650, scoring([`slow`, `slow`, `slow`, `slow`, `slow`, `slow`, `slow`, `slow`, `slow`, `slow`], 3));
+    assert.equal(650, getScoring([
+      {right: true, time: 1},
+      {right: true, time: 7},
+      {right: true, time: 8},
+      {right: true, time: 5},
+      {right: true, time: 3},
+      {right: true, time: 2},
+      {right: true, time: 1},
+      {right: true, time: 7},
+      {right: true, time: 5},
+      {right: true, time: 6}
+    ], 3));
   });
   it(`Если три ответа неправильные, а остальные не быстрые и не медленные, то функция возвращает 700`, () => {
-    assert.equal(700, scoring([`middle`, `fail`, `middle`, `fail`, `middle`, `middle`, `fail`, `middle`, `middle`, `middle`], 0));
+    assert.equal(700, getScoring([
+      {right: true, time: 11},
+      {right: false, time: 17},
+      {right: true, time: 18},
+      {right: true, time: 15},
+      {right: false, time: 13},
+      {right: true, time: 12},
+      {right: true, time: 11},
+      {right: false, time: 17},
+      {right: true, time: 15},
+      {right: true, time: 16}
+    ], 0));
+  });
+});
+
+describe(`getTimer`, () => {
+  it(`При достижении конца таймер должен сообщить о том, что он закончен`, () => {
+    const timer = getTimer(3);
+    timer.tick();
+    timer.tick();
+    assert.equal(`end`, timer.tick());
+  });
+  it(`При каждом обновлении таймера (вызов метода tick) время уменьшается на единицу`, () => {
+    const timer = getTimer(5);
+    assert.equal(4, timer.tick());
+    assert.equal(3, timer.tick());
+    assert.equal(2, timer.tick());
+    assert.equal(1, timer.tick());
   });
 });
